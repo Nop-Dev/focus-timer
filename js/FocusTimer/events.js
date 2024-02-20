2db7c34759c2dc994da3f55149383579d2d01e5a
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-02-20 15:05:09 -0300

    feat: :sparkles: adiciona todos os data-actions de cada botão

--- js/FocusTimer/events.js
+++ js/FocusTimer/events.js
@@ -1,7 +1,13 @@
-import { controls } from "./elements.js"
+import { controls } from "./elements.js";
 
 export function registerControls() {
-    controls.addEventListener('click', (event) => {
-        console.log(event.target)
-    })
-}
+    controls.addEventListener('click', (event) => {    
+        const action = event.target.dataset.action;
+
+        if(action === undefined) {
+            return;
+        };
+
+        console.log(action)
+    });
+};
