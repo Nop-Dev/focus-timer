ceddf3b89c41c6bfb7dd957d9d41922032e9fc7d
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-02-20 14:51:57 -0300

    feat: :construction: cliques dentro dos controles agora são registrados

--- js/FocusTimer/index.js
+++ js/FocusTimer/index.js
@@ -1,8 +1,9 @@
 import state from './state.js'
+import * as events from './EVENTS.JS'
 
-console.log(state);
+export function start(minutes, seconds){
+    state.minutes = minutes
+    state.seconds = seconds
 
-export function start(seconds, minutes) {
-    state.minutes = minutes;
-    state.seconds = seconds;
-};
+    events.registerControls()
+}
